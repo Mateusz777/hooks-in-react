fa456f97d49c920d9e6be040172ed05969350638
Mateusz Szpatusko <esz.mateusz@gmail.com>
2020-01-27 23:23:03 +0100

    add hook that services form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const useForm = email => {
+    const [values, setValues] = useState(email);
+
+    const updateValue = e => {
+        setValues({...values, [e.target.name]: e.target.value})
+    }
+
+    const submitHandler = e => {
+        e.preventDefault();
+        console.log('sending...', values)
+    }
+    return [updateValue, submitHandler];
+}
+
+function App() {
+    const [updateValue, submitHandler] = useForm({});
+
+    return (
+      <div className="App">
+          <h2>example of hook</h2>
+          <form onSubmit={submitHandler}>
+              <input name="email" placeholder="your email" onChange={updateValue}/>
+              <button type="submit">Send</button>
+          </form>
+      </div>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
